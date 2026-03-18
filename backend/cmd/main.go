67e1c84e54437bfc86ec25af7388f5771376fa18
GFR-Q <dikie1920@gmail.com>
2026-03-18 19:05:51 +0500

    Project structure

---
+++ backend/cmd/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Backend for Online Course Platform is starting...")
+}
---
+++ backend/go.mod
@@ -0,0 +1,3 @@
+module github.com/GFR-Q/Online-Course-Platform/backend
+
+go 1.25.6
